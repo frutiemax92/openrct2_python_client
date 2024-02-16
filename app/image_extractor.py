e0f52b5c30cf8bfc41a5ce269eca3f49a27a8516
Lucas Malo Belanger <lucas.malo.belanger@gmail.com>
2024-02-16 16:35:57 -0500

    Add the packing feature for training set

--- app/image_extractor.py
+++ app/image_extractor.py
@@ -21,6 +21,7 @@ def register_image_extractor_block(client : OpenRCT2Client):
                                             label='Object Types', interactive=True)
 
     export_animated_objects = gr.Checkbox(value=False, label='Export Animated Objects')
+    pack_images_checkbox = gr.Checkbox(value=True, label='Pack Views in 512x512 images')
     with gr.Row():
         export_path = gr.Textbox('Export Path', interactive=True, label='Export Path')
         folder_button = gr.Button(FOLDER_SYMBOL)
@@ -30,6 +31,100 @@ def register_image_extractor_block(client : OpenRCT2Client):
             get_file_path,
             outputs=export_path,
         )
+        def pack_images(output_folder, image_index, pack_images : list[Image.Image]):
+            num_images = len(pack_images)
+
+            # this sucks but you need that since the objects delete the palette...
+            palette = Image.open('data/screenshot.png').palette
+
+            if num_images > 4:
+                # this doesn't work for animated scenery!
+                return
+            
+            if num_images == 1:
+                image = pack_images[0]
+
+                # get the largest side
+                largest_side = np.maximum(image.width, image.height)
+
+                # get the scaling ratio
+                scaling = int(512 / largest_side)
+
+                # scale the image
+                image = image.resize((image.width * scaling, image.height * scaling))
+
+                # copy the image into a new 512x512 centered
+                new_image = Image.new(mode='P', size=(512, 512))
+
+                # this is ok to destroy the palette after this
+                new_image.palette = palette
+
+                origin_x = int((512 - image.width) / 2)
+                origin_y = int((512 - image.height) / 2)
+                new_image.paste(image, (origin_x, origin_y))
+
+                out_image = os.path.join(output_folder, f'{image_index}.png')
+                new_image.save(out_image)
+                image_index = image_index + 1
+            elif num_images == 2:
+                resized_images = []
+                for image in pack_images:
+                    # we need to use the smallest ratio
+                    ratio_x = int(256 / image.width)
+                    ratio_y = int(512 / image.height)
+
+                    # get the scaling ratio
+                    scaling = np.minimum(ratio_x, ratio_y)
+
+                    # scale the image
+                    resized_images.append(image.resize((image.width * scaling, image.height * scaling)))
+                
+                # copy the 2 images side by side on a 512x512 image
+                new_image = Image.new(mode='P', size=(512, 512))
+                new_image.palette = palette
+
+                for i in range(2):
+                    origin_x = int((256 - resized_images[i].width) / 2 + 256 * i)
+                    origin_y = int((512 - resized_images[i].height) / 2)
+                    new_image.paste(resized_images[i], (origin_x, origin_y))
+
+                out_image = os.path.join(output_folder, f'{image_index}.png')
+                new_image.save(out_image)
+                image_index = image_index + 1
+            elif num_images == 4:
+                resized_images = []
+                for image in pack_images:
+                    # we need to use the smallest ratio
+                    ratio_x = int(256 / image.width)
+                    ratio_y = int(256 / image.height)
+
+                    # get the scaling ratio
+                    scaling = np.minimum(ratio_x, ratio_y)
+
+                    # scale the image
+                    resized_images.append(image.resize((image.width * scaling, image.height * scaling)))
+
+                # copy the 4 images each in its quadrant
+                new_image = Image.new(mode='P', size=(512, 512))
+                new_image.palette = palette
+
+                i = 0
+                for y in range(2):
+                    for x in range(2):
+                        origin_x = int((256 - resized_images[i].width) / 2 + 256 * x)
+                        origin_y = int((256 - resized_images[i].height) / 2 + 256 * y)
+                        new_image.paste(resized_images[i], (origin_x, origin_y))
+                        i = i + 1
+
+                out_image = os.path.join(output_folder, f'{image_index}.png')
+                new_image.save(out_image)
+
+                new_image.show()
+                image_index = image_index + 1
+
+
+            return image_index
+                
 
         def extract_all_images_of_type(output_folder, type, start_image_index = 0) -> int:
             # first get the ride objects ids
@@ -51,7 +146,12 @@ def register_image_extractor_block(client : OpenRCT2Client):
                     continue
 
                 # parse the images
+                pckg_images = []
+
+                # this avoid multiple copies
+                copied_palette = copy(palette)
                 for image in read_images_result.images:
+                    
                     data = None
                     if image.type == 'rle':
                         data = decode_image_rle(image.data, image.width, image.height)
@@ -62,13 +162,20 @@ def register_image_extractor_block(client : OpenRCT2Client):
                     im = Image.fromarray(data, mode='P')
 
                     # convert the image with the openrct2 palette
-                    im.palette = copy(palette)
+                    im.palette = copied_palette
                     
                     # save the image
-                    out_image = os.path.join(output_path, f'{image_index}.png')
-                    im.save(out_image)
+                    if pack_images_checkbox.value == False:
+                        out_image = os.path.join(output_path, f'{image_index}.png')
+                        im.save(out_image)
 
-                    image_index = image_index + 1
+                        image_index = image_index + 1
+                    pckg_images.append(copy(im))
+                
+                if pack_images_checkbox.value == True:
+                    image_index = pack_images(output_path, image_index, pckg_images)
+                    # we need to pack the images in a 512x512 image
+                    # there is 3 possibilities: 1 view, 2 views and 4 views object
             return image_index
 
         def extract_all_images(output_folder : str):
